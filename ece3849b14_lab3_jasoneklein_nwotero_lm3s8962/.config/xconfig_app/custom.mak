d67d25c13a299f4f00377c37eb1a90f79e8c4182
Jason <jasoneklein5@gmail.com>
2014-12-10 10:47:25 -0500

    Added waveform and draw tasks. lm3s8962 Builds, not tested

--- ece3849b14_lab3_jasoneklein_nwotero_lm3s8962/.config/xconfig_app/custom.mak
+++ ece3849b14_lab3_jasoneklein_nwotero_lm3s8962/.config/xconfig_app/custom.mak
@@ -2,4 +2,4 @@
 .configuro: .libraries,em3 linker.cmd package/cfg/app_pem3.oem3
 
 linker.cmd: package/cfg/app_pem3.xdl
-	$(SED) 's"^\"\(package/cfg/app_pem3cfg.cmd\)\"$""\"C:/Users/Nicholas/Documents/School/ECE 3849/ece3849/ece3849b14_lab3_jasoneklein_nwotero_lm3s8962/.config/xconfig_app/\1\""' package/cfg/app_pem3.xdl > $@
+	$(SED) 's"^\"\(package/cfg/app_pem3cfg.cmd\)\"$""\"C:/Users/Jason Klein/Documents/GitHub/ece3849/ece3849b14_lab3_jasoneklein_nwotero_lm3s8962/.config/xconfig_app/\1\""' package/cfg/app_pem3.xdl > $@
--- ece3849b14_lab3_jasoneklein_nwotero_lm3s8962/main.c
+++ ece3849b14_lab3_jasoneklein_nwotero_lm3s8962/main.c
@@ -125,12 +125,114 @@ void Display_Task(UArg arg0, UArg arg1) {
 
 		FillFrame(0); // clear  OLED frame buffer
 
+		// Decorate screen grids
+		unsigned short k;
+		for (k = 0; k < FRAME_SIZE_X; k += PIXELS_PER_DIV) {
+			unsigned short x = k + 5;
+			unsigned short y = k;
+			DrawLine(x, 0, x, FRAME_SIZE_Y, DIM); // draw vertical gridlines
+			if (y < 96) {
+				DrawLine(0, y, FRAME_SIZE_X, y, DIM); // draw horizontal gridlines
+			}
+		}
+
+		//Draw points using the cached data
+		int j;
+		for (j = 1; j < SCREEN_WIDTH; j++) {
+			// draw data points with lines
+			DrawLine(g_ppWaveformBuffer[j - 1].x, g_ppWaveformBuffer[j - 1].y,
+					g_ppWaveformBuffer[j].x, g_ppWaveformBuffer[j].y, BRIGHT);
+		}
+
+		//Draw selector rectangle
+		unsigned char x1, x2, y1 = 0, y2 = 7;
+		if (g_ucSelectionIndex == 0) {
+			x1 = 0;
+			x2 = 30;
+		} else if (g_ucSelectionIndex == 1) {
+			x1 = 50;
+			x2 = 84;
+		} else {
+			x1 = 100;
+			x2 = 120;
+			y2 = 9;
+		}
+		DrawFilledRectangle(x1, y1, x2, y2, 5);
+
+		//Draw timescale
+		usprintf(pcStr, "%02uus", g_uiTimescale); // convert timescale to string
+		DrawString(5, 0, pcStr, 15, false); // draw string to frame buffer
+
+		//Draw voltage scale
+		usprintf(pcStr, "%02umV", g_uiMVoltsPerDiv); // convert voltage scale to string
+		DrawString(53, 0, pcStr, 15, false); // draw string to frame buffer
+
+		//Draw trigger icon and trigger line
+		drawTrigger(g_iTriggerDirection);
+		int y = (int) ((FRAME_SIZE_Y / 2) - g_iTriggerPixel);
+		DrawLine(0, y, FRAME_SIZE_X - 1, y, 10);
+
 		// copy frame to the OLED screen
 		RIT128x96x4ImageDraw(g_pucFrame, 0, 0, FRAME_SIZE_X, FRAME_SIZE_Y);
 
 	}
 }
 
+/**
+ * Task Function: Waveform_Task
+ * ----------------------------
+ *   Processes ADC buffer, g_pusADCBuffer, and sets g_ppWaveformBuffer for
+ *   Display_Task() and FFT_Task().
+ *   Pends on Wave_Sem. Upon loop iteration completion, posts to
+ *   Draw_Sem or FFT_Sem if FFT mode is disabled or enabled respectively.
+ *
+ *   returns: nothing
+ */
+void Waveform_Task(UArg arg0, UArg arg1) {
+	float fScale; // scaling factor to map ADC counts to pixels
+
+	for (;;) {
+		// Pend on sempaphore
+		Semaphore_pend(Wave_Sem, BIOS_WAIT_FOREVER);
+
+		//Determine scaling factor
+		fScale = ((float) (VIN_RANGE * 1000 * PIXELS_PER_DIV))
+				/ ((float) ((1 << ADC_BITS) * g_uiMVoltsPerDiv));
+
+		//Find trigger
+		float triggerLevel = (3.0 / (1 << ADC_BITS))
+				* (g_iTriggerPixel / fScale);
+		int triggerIndex = triggerSearch(triggerLevel, g_iTriggerDirection,
+				SCREEN_WIDTH);
+
+		//Copy NFFT, 1024 samples, into local buffer
+		Point tempBuffer[SCREEN_WIDTH];
+		int i;
+		for (i = 0; i < SCREEN_WIDTH; i++) {
+			Point dataPoint;
+			dataPoint.x = i;
+			dataPoint.y =
+					g_pusADCBuffer[ADC_BUFFER_WRAP(triggerIndex + i - SCREEN_WIDTH/2)];
+			tempBuffer[i] = dataPoint;
+		}
+
+		int j;
+		for (j = 1; j < SCREEN_WIDTH; j++) {
+			int adcY0 = 2
+					* (tempBuffer[j - 1].y - ((1 << ADC_BITS) / 3.0) * (1.5)); //Scale previous sample
+			int adcY1 = 2 * (tempBuffer[j].y - ((1 << ADC_BITS) / 3.0) * (1.5)); //Scale current sample
+
+			g_ppWaveformBuffer[j - 1].y = FRAME_SIZE_Y / 2
+					- ((adcY0 - ADC_OFFSET) * fScale);
+			g_ppWaveformBuffer[j - 1].x = tempBuffer[j - 1].x;
+			g_ppWaveformBuffer[j].y = FRAME_SIZE_Y / 2
+					- ((adcY1 - ADC_OFFSET) * fScale);
+			g_ppWaveformBuffer[j].x = tempBuffer[j].x;
+		}
+
+		Semaphore_post(Draw_Sem);
+	}
+}
 
 /**
  * Function: adcSetup
@@ -176,4 +278,78 @@ void buttonSetup(void) {
 	GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_1); // set GPIO pin 1 as input
 	GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_STRENGTH_2MA,
 			GPIO_PIN_TYPE_STD_WPU);
+}
+
+/**
+ * Function: triggerSearch
+ * ----------------------------
+ *   This function searches for a trigger in the ADC buffer and returns the index of the trigger
+ *   when found. If one is not found, this function will return the index half a buffer away from the newest sample
+ *
+ *   triggerLevel: the floating-point magnitude, in Volts, of the trigger
+ *   direction: the direction of change of a trigger, either (int) +1 for rising or -1 for falling
+ *   samples: the number of samples to search through
+ *
+ *   returns: the index of the trigger, or the index half a buffer from the newest sample if a trigger is not found
+ */
+unsigned int triggerSearch(float triggerLevel, int direction, int samples) {
+	unsigned int startIndex = g_iADCBufferIndex - (samples / 2); //start half a screen width from the most recent sample
+	unsigned int searchIndex = startIndex;
+	unsigned int searched = 0; //This avoids doing math dealing with the buffer wrap for deciding when to give up
+	//	unsigned int triggerLevelAdc = (((triggerLevel/2.0)+1.5)/3.0)*(1 << ADC_BITS);
+	unsigned int triggerLevelAdc = ((int) (triggerLevel * 170.667)) + 512;
+
+	while (1) {
+		//Look for trigger,break loop if found
+		unsigned int curCount = g_pusADCBuffer[searchIndex]; //Accessing two members of a shared data structure non-atomically
+		unsigned int prevCount = g_pusADCBuffer[searchIndex - 1]; //Shared data safe because ISR writes to different part of buffer
+		//		float curVolt = ADC_TO_VOLT(curCount);
+		//		float prevVolt = ADC_TO_VOLT(prevCount);
+
+		//Is curVolt a trigger?
+		if (((direction == 1) && 					//If looking for rising edge
+				(curCount >= triggerLevelAdc) && (prevCount < triggerLevelAdc))	//And the current and previous voltages are above/equal to and below the trigger
+				|| ((direction == -1)
+						&& 			//Or, if looking for falling edge
+						(curCount <= triggerLevelAdc)
+						&& (prevCount > triggerLevelAdc))) { //And the current and previous voltages are below/equal to and above the trigger
+			return searchIndex;
+		}
+
+		//If we have searched half of the buffer, give up. Else, move the search index back
+		if (searched == (ADC_BUFFER_SIZE / 2)) {
+			g_ulTriggerSearchFail++;
+			return ADC_BUFFER_WRAP(g_iADCBufferIndex - (ADC_BUFFER_SIZE / 2));;
+		} else {
+			searchIndex = ADC_BUFFER_WRAP(--searchIndex);
+			searched++;
+		}
+	}
+}
+
+/**
+ * Function: drawTrigger
+ * ----------------------------
+ *   This function draws the trigger icon which depicts the direction of change being used for the
+ *   triggerSearch function
+ *
+ *   direction: the direction of change of a trigger, either (int) +1 for rising or -1 for falling
+ *
+ *   returns: nothing
+ */
+void drawTrigger(int direction) {
+	DrawLine(TRIGGER_X_POS - (direction * TRIGGER_H_LINE),
+			TRIGGER_Y_POS + TRIGGER_V_LINE, TRIGGER_X_POS,
+			TRIGGER_Y_POS + TRIGGER_V_LINE, BRIGHT); // draw bottom horizontal line
+	DrawLine(TRIGGER_X_POS, TRIGGER_Y_POS + TRIGGER_V_LINE, TRIGGER_X_POS,
+			TRIGGER_Y_POS - TRIGGER_V_LINE, BRIGHT); // draw vertical line
+	DrawLine(TRIGGER_X_POS, TRIGGER_Y_POS - TRIGGER_V_LINE,
+			TRIGGER_X_POS + (direction * TRIGGER_H_LINE),
+			TRIGGER_Y_POS - TRIGGER_V_LINE, BRIGHT); // draw top horizontal line
+	DrawLine(TRIGGER_X_POS - TRIGGER_ARROW_WIDTH,
+			TRIGGER_Y_POS + (direction * TRIGGER_ARROW_HEIGHT), TRIGGER_X_POS,
+			TRIGGER_Y_POS, BRIGHT); // draw left side of arrow
+	DrawLine(TRIGGER_X_POS + TRIGGER_ARROW_WIDTH,
+			TRIGGER_Y_POS + (direction * TRIGGER_ARROW_HEIGHT), TRIGGER_X_POS,
+			TRIGGER_Y_POS, BRIGHT); // draw right side of arrow
 }
